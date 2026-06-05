e5e91f794ce4cd5e32c706cdec1269aaea0a59ca
Chandrapal Singh <chandrapal1990@gmail.com>
2026-06-05 15:55:32 +0530

    created main file for transfer learning using VGG, ResNEt and EfficientNet

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ print(device)
 train_transform = transforms.Compose([
     transforms.Resize((224,224)),
     transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.RandomAffine(
+        degrees=10,
+        translate=(0.1, 0.1),
+        scale=(0.9, 1.1)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
@@ -49,28 +54,68 @@ print("Test Dataset Loaded")
 
 # create loader 
 
-train_loader = DataLoader(train_dataset, batch_size=8,shuffle=True)
-test_loader = DataLoader(test_dataset,batch_size=8,shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=16,shuffle=True)
+test_loader = DataLoader(test_dataset,batch_size=16,shuffle=False)
 
 
 
 # pre-trained 1 vgg
 
-model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+# model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
 
-print(model)
+# model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
+# print(model)
 
-for param in model.features[24].parameters():
-    param.requires_grad = True
+# for param in model.features.parameters():
+#     param.requires_grad = False
+
+
+# for param in model.features[24:].parameters(): for vgg 
+#     param.requires_grad = True
     
 
-num_feature = model.classifier[6].in_features
+# for param in model.features[5:].parameters():
+#     param.requires_grad = True
+    
+
+# num_feature = model.classifier[1].in_features
 
 
-model.classifier[6]= nn.Linear(num_feature,config['num_class'])
+# model.classifier[1]= nn.Linear(num_feature,config['num_class'])  base classifier , now in this iw ill add dropout using sequential 
 
-print(model.classifier)
+# model.classifier= nn.Sequential(
+#     nn.Dropout(0.5),
+#     nn.Linear(num_feature,config['num_class']))  # replacing the classifier structure thats why no model.classifier[1]
 
+# print(model.classifier)
+
+
+##############
+###ResNet
+
+model = models.resnet50(weights =models.ResNet50_Weights.DEFAULT)
+print(model)
+
+for param in model.parameters():
+    param.requires_grad=False
+
+
+for param in model.layer3.parameters():
+    param.requires_grad=True
+
+for param in model.layer4.parameters():
+    param.requires_grad=True
+  
+num_feature = model.fc.in_features
+
+# model.fc= nn.Linear(num_feature,4)  # replacing the classifier structure thats why no model.classifier[1]
+
+model.fc = nn.Sequential(
+    nn.Dropout(0.5),
+    nn.Linear(num_feature, 4)
+)
+
+print(model.fc)
 
 model= model.to(device)
 print(model)
@@ -78,7 +123,19 @@ print(model)
 
 ## loss function and optimizer
 criterion =nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),lr= config['learning_rate'],  weight_decay=float(config['weight_decay']))
+# optimizer = torch.optim.Adam(model.parameters(),lr= config['learning_rate'],  weight_decay=float(config['weight_decay']))
+optimizer = torch.optim.Adam(
+    filter(lambda p: p.requires_grad,
+           model.parameters()),
+    lr=0.0001,
+    weight_decay=1e-4
+)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer,
+    mode='max',
+    factor=0.1,
+    patience=2
+)
 
 #veryfy
 
@@ -137,17 +194,20 @@ for epoch in range(config['num_epochs']):
             val_correct += (predicted==labels).sum().item()
     validation_loss = val_loss / len(test_loader)
     val_acc = 100 * val_correct / val_total
+    scheduler.step(val_acc)
+    current_lr = optimizer.param_groups[0]['lr']
     
     print(
-        f"Epoch [{epoch+1}/{config['num_epochs']}]"
+        f"Epoch [{epoch+1}/{config['num_epochs']}] "
+        f"LR: {current_lr:.6f} "
         f"Train Loss: {train_loss:.4f} "
-        f"train acc : {train_acc:.2f}% "
-        f"val loss: {validation_loss:.4f} "
-        f"val acc:{val_acc:.2f}% "
+        f"Train Acc: {train_acc:.2f}% "
+        f"Val Loss: {validation_loss:.4f} "
+        f"Val Acc: {val_acc:.2f}%"
     )
     if val_acc>best_acc:
         best_acc=val_acc
         torch.save(
-            model.state_dict(), "best_vgg.pth"
+            model.state_dict(), "best_resnet.pth"
         )
         print("best model saved")
